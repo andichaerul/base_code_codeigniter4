1104909c09e793de2848b04777f287d4a3920557
Andi Chaerul <andichaerul85@gmail.com>
2022-03-15 00:49:08 +0800

    Commit All From Host

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,1 +1,11 @@
-xxx
+<?= $this->extend('layouts/v_header') ?>
+<?php $this->section('content') ?>
+<section class="section">
+    <div class="section-header">
+        <h1>Blank Page</h1>
+    </div>
+
+    <div class="section-body">
+    </div>
+</section>
+<?php $this->endSection('content') ?>
